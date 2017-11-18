cf7b9954e1b4a830db4949170911730c7a81a97c
feke9432 <feke9432@gmail.com>
2017-11-18 21:02:47 +0800

    提出配置文件，批量改多条内容

--- node/bulkReplaceCont/app.js
+++ node/bulkReplaceCont/app.js
@@ -1,7 +1,51 @@
-var bulkReplaceCont = require('./bulkReplaceCont')
+const fs = require('fs');
+const config = require('./config');
 
-bulkReplaceCont({
-    rootPath: 'test',
-    oldStr: '改之前的内容，支持正则和字符串',
-    newStr: '改之后的内容，注意，过程不可变，请做好备份'
-});
+function readDir () {
+    return new Promise((resolve, reject) => {
+        fs.readdir(__dirname + '/test', (err, files) => {
+            if (err) return console.log(err);
+            resolve(files);
+        });
+    });
+}
+
+function readFlie (url) {
+    return new Promise((resolve, reject) => {
+        var pathStr = __dirname + '/test/' + url + '/index.html';
+        fs.readFile( pathStr, { encoding: 'utf-8' }, (err, data) => {
+            if (err) return console.log(err);
+            var cData = data;
+
+            for (let i in config) {
+                loop(config[i]);
+            }
+
+            function loop(opts) {
+                if (cData.indexOf(opts.oldStr) >= 0) {
+                    cData = cData.replace(opts.oldStr, opts.newStr);
+                    loop(opts)
+                }
+            }
+
+            fs.writeFile(pathStr, cData);
+            resolve(data);
+        })
+    });
+}
+
+function brandFile(files) {
+    var pArr = [];
+    // return readFlie(files[0])
+    for (var f of files) {
+        pArr.push(readFlie(f));
+    }
+
+    return Promise.all(pArr);
+}
+
+(async () => {
+    var files = await readDir();
+    // readFlie(files[0])
+    var htmlArr = await brandFile(files);
+})();
---
+++ node/bulkReplaceCont/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    qqObj : {
+        oldStr: '657457337',
+        newStr: '新群号码'
+    },
+    urlObj: {
+        oldStr: 'https://jq.qq.com/?_wv=1027&k=5UPnXsJ',
+        newStr: '新的链接'
+    }
+}
